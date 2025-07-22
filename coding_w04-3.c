d1fd77a57f84a090b0d94ad693aa076eb7316822
SupphawitJantummee <s6812022611090@email.kmutnb.ac.th>
2025-07-22 17:19:07 +0700

    Supphawit

---
+++ coding_w04-3.c
@@ -0,0 +1,15 @@
+#include  <stdio.h>
+
+int main() {
+    char firstInitial = 'K';
+    int age = 18;
+    float weight = 100.0;
+    char gender[] = "boy";
+
+    printf("nameInitial: %c\n", firstInitial );
+    printf("Age: %d\n", age);
+    printf("Weight: %.1f kg.\n", weight);
+    printf("Gender: %s\n", gender);
+    return 0;
+    }
+   
